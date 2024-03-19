6504defd319896287172045f7586a71861c7a3c7
StavWasPlayZ <stavsolomon101@gmail.com>
2024-03-19 20:36:57 +0200

    Update Da Rules

--- src/App.js
+++ src/App.js
@@ -11,9 +11,7 @@ export default function App()
         </div>
         <div>
           <h2>Da Rules:</h2>
-          <p>1. The probabilities should add up to 1.</p>
-          <p>2. A probability should be in the range of 0-1.</p>
-          <p>3. The probability number should be up to 2 numbers after the decimal point (for example: 0.45).</p>
+          <p>#yolo</p>
         </div>
       </div>
     );
